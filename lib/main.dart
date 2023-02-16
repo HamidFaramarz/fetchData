81911d0cbc5a752d66c157b474eae360dfa98e47
HamidFaramarz <hamidullah.faramarz2020@gmail.com>
2023-02-16 19:25:42 +0430

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,90 @@
+import 'dart:convert';
+import 'package:diotest/next.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-
-import 'homepage.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dio Networking',
+    return GetMaterialApp(
+      title: 'Flutter API Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic>? _users;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
+  }
+
+  Future<void> _fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
+    final data = jsonDecode(response.body);
+    setState(() {
+      _users = data['data'];
+    });
+  }
+
+  Widget _buildList() {
+    if (_users == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return ListView.builder(
+      itemCount: _users!.length,
+      itemBuilder: (context, index) {
+        final user = _users![index];
+        return Card(
+          color: Colors.lightBlue[100],
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(user['avatar']),
+            ),
+            title: Text('${user['first_name']} ${user['last_name']}'),
+            subtitle: Text(user['email']),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Users'),
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                Get.to(() => const NextPage());
+              },
+              child: const Text("Next"))
+        ],
+      ),
+      body: _buildList(),
     );
   }
 }
---
+++ lib/next.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:diotest/page3.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  List<dynamic>? _userProfile;
+  // function to get image
+
+  Future<void> fetchUserProfile() async {
+    var response =
+        await http.get(Uri.parse("https://reqres.in/api/users?page=2"));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        _userProfile = data['data'];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    fetchUserProfile();
+    super.initState();
+  }
+
+  Widget _buildMyList() {
+    if (_userProfile == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return ListView.builder(
+      itemCount: _userProfile!.length,
+      itemBuilder: (context, index) {
+        final userProfile = _userProfile![index];
+        return Card(
+          color: Colors.lightBlue[100],
+          child: ListTile(
+            leading: CircleAvatar(
+                backgroundImage: NetworkImage(userProfile['avatar'])),
+            title: Text(
+                "${userProfile['first_name']} - ${userProfile['last_name']}"),
+            subtitle: Text("${userProfile['email']}"),
+            trailing: Text("${userProfile['id']}"),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                // Get.to(() => PageThree());
+              },
+              child: Text("page 3"))
+        ],
+        title: const Text(
+          "Next Page",
+          style: TextStyle(color: Colors.amberAccent),
+        ),
+        centerTitle: true,
+      ),
+      body: _buildMyList(),
+    );
+  }
+}
